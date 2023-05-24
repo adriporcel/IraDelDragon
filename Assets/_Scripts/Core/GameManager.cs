e298b3d38fb27179a5d4cc8a630e84063ac2952e
Adrian <adriporcel90@gmail.com>
2023-05-24 12:14:12 +0200

    Player health system and a couple of things from the turn system that were required for the GameOver check implementation

--- Assets/_Scripts/Core/GameManager.cs
+++ Assets/_Scripts/Core/GameManager.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public bool DebugMode { get { return debugMode; } }
+    public int MainPlayerHealth { get { return mainPlayerHealth; } }
+    public int SecondaryPlayerHealth { get { return secondaryPlayerHealth; } }
 
     public static GameManager instance;
 
     public GameState gameState;
     public GameObject selectedCard;
+    public int mainPlayerInitialHealth, secondaryPlayerInitialHealth; // Game default is 21
 
     [SerializeField] bool debugMode;
     [SerializeField] DeckController deckController;
+    [SerializeField] int mainPlayerHealth, secondaryPlayerHealth;
 
     void Awake()
     {
@@ -35,28 +39,57 @@ public class GameManager : MonoBehaviour
             UnityEditor.EditorWindow.FocusWindowIfItsOpen(typeof(UnityEditor.SceneView));
         }
 
-        deckController.InitialDeal(); // DEBUG remove
         print($"Debug mode set to: {debugMode}"); // DEBUG
+        gameState = GameState.start; // DEBUG
     }
 
     private void Update()
     {
-        // TODO: implement states
+        switch (gameState)
+        {
+            case GameState.start:
+                GameStartSetHealth(); // Set players max health
+                deckController.InitialDeal(); // Deal initial number of cards to players hands
+                gameState = GameState.mainPlayerTurn; // main player will begin his turn
+                break;
 
-        //switch (gameState)
-        //{
-        //    case GameState.start:
-        //        deckController.InitialDeal();
-        //        break;
-        //    case GameState.mainPlayerTurn:
-        //        break;
-        //    case GameState.secondPlayerTurn:
-        //        break;
-        //    case GameState.endGame:
-        //        break;
-        //    default:
-        //        break;
-        //}
+            case GameState.mainPlayerTurn:
+                GameOverCheck();
+                break;
+
+            case GameState.secondPlayerTurn:
+                GameOverCheck();
+                break;
+
+            case GameState.endGame:
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    /// <summary>
+    /// Called once at the start of the game, set's player starting health points
+    /// </summary>
+    void GameStartSetHealth()
+    {
+        mainPlayerHealth = mainPlayerInitialHealth;
+        secondaryPlayerHealth = secondaryPlayerInitialHealth;
+    }
+
+    void GameOverCheck()
+    {
+        if (MainPlayerHealth <= 0)
+        {
+            print($"{Players.main} lost the game");
+            gameState = GameState.endGame;
+        }
+        else if (SecondaryPlayerHealth <= 0)
+        {
+            print($"{Players.secondary} lost the game");
+            gameState = GameState.endGame;
+        }
     }
 
     public void SelectCard(GameObject card)
